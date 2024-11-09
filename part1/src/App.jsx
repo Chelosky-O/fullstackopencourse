91257477d9aaf2350764c0d30772da36cb0edab6
Chelosky-O <marcelofernando.mv@gmail.com>
2024-11-09 17:50:28 -0300

    part 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -8,16 +8,9 @@ const Header = (props) => {
 const Content = (props) => {
   return(
     <div>
-      <p>
-        {props.part1} {props.exercise1}
-      </p>
-      <p>
-        {props.part2} {props.exercise2}
-      </p>
-      <p>
-        {props.part3} {props.exercise3}
-      </p>
-
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
     </div>
     
   )
@@ -27,6 +20,14 @@ const Total = (props) => {
   return(
     <p>
     Number of exercises {props.total}
+    </p>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <p>
+        {props.part} {props.exercises}
     </p>
   )
 }
@@ -43,7 +44,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercise1={exercises1} part2={part2} exercise2={exercises2} part3={part3} exercise3={exercises3}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
       <Total total={exercises1+exercises2+exercises3}/>
     </div>
   )
