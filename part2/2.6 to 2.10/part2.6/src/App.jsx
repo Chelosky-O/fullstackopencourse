e667e53cbd0ee20df56fac4320dac4e9ecf17bba
Chelosky-O <marcelofernando.mv@gmail.com>
2025-01-19 16:07:25 -0300

    2.10

--- part2/2.6 to 2.10/part2.6/src/App.jsx
+++ part2/2.6 to 2.10/part2.6/src/App.jsx
@@ -1,88 +1,64 @@
-import { useState } from 'react'
-
-const Person = ({ person, number }) => {
-  return (
-    <li>{person} {number}</li>
-  )
-}
-
-
+import { useState } from "react";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [newSearch, setNewSearch] = useState('')
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newSearch, setNewSearch] = useState("");
 
   const addPerson = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    if(persons.some((person) => person.name === newName)){
-      alert(`${newName} is already added to phonebook`)
-    }else{
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
       const personObject = {
         name: newName,
         number: newNumber,
-      }
-      setPersons(persons.concat(personObject))
-      setNewName('')
+      };
+      setPersons(persons.concat(personObject));
+      setNewName("");
+      setNewNumber("");
     }
-  }
+  };
 
-  const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
+  const handleNameChange = (event) => setNewName(event.target.value);
 
-  const handleNumberChange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
-  }
+  const handleNumberChange = (event) => setNewNumber(event.target.value);
 
-  const handleSearchChange = (event) => {
-    console.log(event.target.value)
-    setNewSearch(event.target.value)
-  }
+  const handleSearchChange = (event) => setNewSearch(event.target.value);
 
-
-  const personsToShow = newSearch == "" ? persons : persons.filter(person => person.name.toLowerCase().includes(newSearch.toLowerCase()))
+  const personsToShow =
+    newSearch === ""
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().includes(newSearch.toLowerCase())
+        );
 
   return (
     <div>
       <h2>Phonebook</h2>
-      filter shown with <input
-          value={newSearch}
-          onChange={handleSearchChange}
-        />
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input
-            value={newName}
-            onChange={handleNameChange}
-           />
-        </div>
-        <div>number: <input
-          value={newNumber}
-          onChange={handleNumberChange}
-        /></div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {personsToShow.map(person => 
-          <Person key={person.name} person={person.name} number={person.number}/>
-        )}
-      </ul>
+      <Filter newSearch={newSearch} handleSearchChange={handleSearchChange} />
+      <h3>Add a new</h3>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h3>Numbers</h3>
+      <Persons personsToShow={personsToShow} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
