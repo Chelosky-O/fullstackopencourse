94e89eec2e1b67694db7c23c4f5d8b3874259a48
Chelosky-O <marcelofernando.mv@gmail.com>
2025-01-20 19:17:13 -0300

    2.15

--- part2/2.11/2.11/src/App.jsx
+++ part2/2.11/2.11/src/App.jsx
@@ -3,6 +3,8 @@ import axios from 'axios'
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import personService from './services/persons'
+
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -11,29 +13,79 @@ const App = () => {
   const [newSearch, setNewSearch] = useState("");
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
+
+
   const addPerson = (event) => {
     event.preventDefault();
-
+    const existingPerson  = persons.find((person) => person.name === newName)
     if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
+      if(window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)){
+        
+        const personObject = {
+          ...existingPerson,
+          number: newNumber,
+        };
+
+
+        personService
+        .update(existingPerson.id, personObject)
+          .then(returnedPerson => {
+            setPersons(
+              persons.map((person) =>
+                person.id !== existingPerson.id ? person : returnedPerson
+              )
+            );
+          setNewName("");
+          setNewNumber("");
+      })
+      }
     } else {
       const personObject = {
         name: newName,
         number: newNumber,
       };
       setPersons(persons.concat(personObject));
-      setNewName("");
-      setNewNumber("");
+
+
+      personService
+      .create(personObject)
+        .then(returnedNote => {
+        setPersons(persons.concat(returnedNote))
+        setNewName("");
+        setNewNumber("");
+      })
+
     }
   };
+
+  const deletePerson = (event,id) => {
+    event.preventDefault();
+    const url = `http://localhost:3001/persons/${id}`
+    const person = persons.find(n => n.id === id)
+    console.log("hola")
+  
+    personService
+      .delete_id(url)
+        .then(()   => {
+          setPersons(persons.filter((n) => n.id !== id));
+          console.log("Person deleted successfully");
+      })
+      .catch(error => {
+        alert(
+          `the note '${person.content}' was already deleted from server`
+        )
+        setPersons(persons.filter(n => n.id !== id))
+      })
+  }
+
+
+  
 
   const handleNameChange = (event) => setNewName(event.target.value);
 
@@ -61,7 +113,7 @@ const App = () => {
         handleNumberChange={handleNumberChange}
       />
       <h3>Numbers</h3>
-      <Persons personsToShow={personsToShow} />
+      <Persons personsToShow={personsToShow} deletePerson={deletePerson}/>
     </div>
   );
 };
--- part2/2.11/2.11/src/components/Persons.jsx
+++ part2/2.11/2.11/src/components/Persons.jsx
@@ -1,14 +1,23 @@
-const Persons = ({ personsToShow }) => {
-    return (
-      <ul>
-        {personsToShow.map((person) => (
-          <li key={person.name}>
-            {person.name} {person.number}
-          </li>
-        ))}
-      </ul>
-    );
+const Persons = ({ personsToShow, deletePerson }) => {
+  const handleSubmit = (event, person) => {
+    event.preventDefault(); // Evita el comportamiento por defecto del formulario
+    if (window.confirm(`Delete ${person.name}?`)) {
+      deletePerson(event, person.id); // Llama a la función deletePerson pasando el evento y el ID
+    }
   };
-  
-  export default Persons;
-  
+
+  return (
+    <ul>
+      {personsToShow.map((person) => (
+        <li key={person.name}>
+          <form onSubmit={(event) => handleSubmit(event, person)}>
+            {person.name} {person.number}{' '}
+            <button type="submit">delete</button>
+          </form>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Persons;
